62ddb482d36af8a54838c76751b8085ef48be07c
uddeshyac9 <uddeshyac9@gmail.com>
2023-12-23 22:54:48 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import './index.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router basename="/study-notion/">
+  <Router basename="/study-notion">
     <App />
     <Toaster/>
   </Router>,
